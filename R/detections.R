bd120e8308da3b6952d2f40052a96b4275070dd1
LukeHearon <luke.e.hearon@gmail.com>
2024-10-23 22:10:12 -0400

    improve detection handling just sorta all around

--- R/detections.R
+++ R/detections.R
@@ -21,6 +21,8 @@ read_raw <- function(path_raw){
 #' @return A data.table
 #' @export
 bin_raw <- function(dt_raw, thresholds=c('ins_buzz'=-1), binwidth=5, time_start=NA){
+  dt_raw <- data.table::as.data.table(dt_raw)
+
   # TODO: give warning when time_start and $start_real both exist
   if(is.null(dt_raw$start_real)){
     if(is.na(time_start)){stop('input data has no start_real column, so you must provide a value for time_start')}
@@ -40,7 +42,7 @@ bin_raw <- function(dt_raw, thresholds=c('ins_buzz'=-1), binwidth=5, time_start=
     .SDcols = names(thresholds)
   ]
 
-  names(dt_bin) <- c('start_bin', 'frames', names(thresholds))
+  names(dt_bin) <- c('start_bin', 'frames', paste0('detections_', names(thresholds)))
 
   return(dt_bin)
 }
@@ -54,14 +56,31 @@ bin_raw <- function(dt_raw, thresholds=c('ins_buzz'=-1), binwidth=5, time_start=
 #' @export
 rebin <- function(dt_bin, binwidth){
   # TODO: catch when input binwidth is less than existing binwidth
+  if(is.null(dt_bin$start_bin)){
+    stop('input data has no start_bin value')
+  }
+
+  if(length(grep("^detections_", names(dt_bin)))==0){
+    stop('input data has no detections_ columns')
+  }
+
+  dt_bin <- data.table::as.data.table(dt_bin)
+  if(!lubridate::is.POSIXct(dt_bin$start_bin)){fasttime::fastPOSIXct(dt_bin$start_bin)}
+
   dt_rebin <- dt_bin
   dt_rebin$start_bin <- lubridate::floor_date(dt_bin$start_bin, unit = paste0(binwidth, 'minutes'))
+
+  value_cols <- c('frames', grep("^detections_", names(dt_rebin), value = TRUE))
 
   dt_rebin <- dt_rebin[
     ,
     lapply(.SD, sum),
-    by = start_bin,
-    .SDcols = !c('start_bin')
+
+    # group by non-values
+    by = setdiff(names(dt_rebin), value_cols),
+
+    # summarize values
+    .SDcols = value_cols
   ]
 
   return(dt_rebin)
