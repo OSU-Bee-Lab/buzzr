eef8e8556ad204bc594b43eea6d3326f65462e5c
LukeHearon <luke.e.hearon@gmail.com>
2025-05-21 17:07:59 -0400

    add bin_experiment export

--- R/results.R
+++ R/results.R
@@ -144,10 +144,11 @@ bin_recorder <- function(dir_recorder, intermediate_dirs = NULL, thresholds=c(in
 }
 
 
-#' Read and join all result files in a direct4ory, appending the names of directories between the dir_experiment and the recorder_dir.
+#' Read and join all result files in a directory, appending the names of directories between the dir_experiment and the recorder_dir.
 #'
 #' @param dir_experiment The directory holding all buzzdetect results to be analyzed.
 #' @inheritParams bin_recorder
+#' @export
 bin_experiment <- function(dir_experiment, intermediate_dirs, thresholds=c(ins_buzz=0), binwidth=5, results_tag="_buzzdetect", append_path=T){
   paths_results <- list_matching_tag(dir_experiment, results_tag)
   dirs_recorders <- unique(dirname(paths_results))
