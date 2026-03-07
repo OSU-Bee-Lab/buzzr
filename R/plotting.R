7b80db7af838e172db85b842ad29c727e5ec03f4
luke <luke.e.hearon@gmail.com>
2026-03-07 01:04:59 -0500

    fix plot scaling issues

--- R/plotting.R
+++ R/plotting.R
@@ -37,95 +37,58 @@ palette <- {
 #'
 #' @returns A ggplot2 theme
 #' @export
-theme_buzzr <- function(mode='light'){
+theme_buzzr <- function(base_size=10, mode='light'){
   if(mode=='light'){
-    return(theme_buzzr_light())
+    return(theme_buzzr_light(base_size))
   }
 
   if (mode == 'dark'){
-    return(theme_buzzr_dark())
+    return(theme_buzzr_dark(base_size))
   }
 
   stop('Theme mode must be light or dark')
 }
 
-theme_buzzr_light <- function(base_size=10){
+theme_buzzr_light <- function(base_size){
   ggplot2::theme_minimal(base_size) +
-  ggplot2::theme(
-    # Parent text settings
-    #
-      text = ggplot2::element_text(family="Futura-Medium", size=ggplot2::rel(5)),
+    ggplot2::theme(
 
-    # Plot
-    #
-      plot.background = ggplot2::element_rect(linewidth=0),  # remove border
-      plot.title = ggplot2::element_text(
-        hjust = 0.5,
-        size=ggplot2::rel(6), # for some reason, this needs to be set independently of "text"
-        margin = ggplot2::margin(b=base_size/5, unit='lines')
-      ),
+      text = ggplot2::element_text(family="Futura-Medium", size=base_size),
 
-      # plot.margin = ggplot2::margin(
-      #   t=base_size*8,
-      #   r=base_size*9,
-      #   b=base_size*4,
-      #   l=base_size*5
-      # ),
+      # Plot
+      plot.background = ggplot2::element_rect(linewidth=0),  # remove border from plots (panels still have them)
+      plot.title = ggplot2::element_text(hjust = 0.5),
 
-    # Panel
-    #
+      # Panel
       panel.border = ggplot2::element_rect(linewidth=1, color = 'black', fill='transparent'),
       panel.ontop = F,
-      panel.spacing.y = ggplot2::unit(base_size/4, 'lines'),
 
-
-      # horizontal
+      # Gridlines
       panel.grid.major.y = ggplot2::element_line(linewidth = ggplot2::rel(0.8)),
       panel.grid.minor.y = ggplot2::element_line(linewidth = ggplot2::rel(0)),
 
-      # vertical
       panel.grid.major.x = ggplot2::element_line(linewidth = ggplot2::rel(1.5)),
       panel.grid.minor.x = ggplot2::element_line(linewidth = ggplot2::rel(0)),
 
-    # Axes
-    #
+      # Axes
       axis.title.y = ggplot2::element_text(
-        angle=0,
-        hjust=1,
-        vjust=0.5,
-        margin = ggplot2::margin(r=ggplot2::rel(25))
+        angle=0,  # read without tilting your head
+        hjust=1,  # right align
+        vjust=0.5, # vertical center
+        margin=margin(r=base_size)
       ),
 
-      axis.title.x = ggplot2::element_text(
-        hjust=0.5,
-        margin = ggplot2::margin(t=ggplot2::rel(15))
-      ),
+      axis.title.x = ggplot2::element_text(hjust=0.5),
 
-
-      axis.text = ggplot2::element_text(
-        margin=ggplot2::margin(
-          l=15*base_size,
-          r=15*base_size,
-          t=15*base_size,
-          b=15*base_size
-        )
-      ),
-
-    # Legend
-    #
-      legend.text = ggplot2::element_text(size=ggplot2::rel(4.5)),  # Why does this need to be set separately from text?
-
-    # Facets/strips
-    #
-    # strip.text.y appears to be get two updates, one from the base "text" setting and again here;
-    # however, strip.text.x doesn't? So only set x.
-    strip.text.x = ggplot2::element_text(size=rel(4), angle=0),
-    strip.text.y = ggplot2::element_text(angle=0, hjust=0)
-  )
+      # Strips (facet_grid, facet_wrap)
+      panel.spacing.y = unit(12, 'pt'),
+      strip.text.x = ggplot2::element_text(angle=0),
+      strip.text.y = ggplot2::element_text(angle=0, hjust=0)
+    )
 }
 
 
-theme_buzzr_dark <- function(base_size=11){
+theme_buzzr_dark <- function(base_size){
   theme_buzzr_light(base_size) +
   ggplot2::theme(
     # Parent text settings
